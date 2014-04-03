fe35673c859703cdc11e0c690a74044f7e0ad990
Grant Mather <hcaulfield57@gmail.com>
2014-04-03 17:47:11 -0600

    cp should be working now

--- cp.hs
+++ cp.hs
@@ -1,10 +1,10 @@
-{- BUGS: No error handling of any kind
- - Breaks if user supplies trailing / -}
-
 module Main where
 
+import Control.Exception
 import Control.Monad
+import Control.Monad.Error
 import Control.Monad.Reader
+import Control.Monad.Trans
 import System.Console.GetOpt
 import System.Directory
 import System.Environment
@@ -15,46 +15,54 @@ import Text.Printf
 data Flag = Verbose | Recurse
     deriving Eq
 
+type CopyMonad = ReaderT [Flag] (ErrorT IOException IO) ()
+
 flags :: [OptDescr Flag]
 flags = [Option "v" [] (NoArg Verbose) [],
          Option "rR" [] (NoArg Recurse) []]
 
-copy :: [String] -> String-> ReaderT [Flag] IO ()
+copy :: [String] -> String -> CopyMonad 
 copy [] _ = return ()
-copy s@(src:srcs) dst = do
-    isSrcFile <- lift $ doesFileExist src
-    isDstFile <- lift $ doesFileExist dst
-    isSrcDir <- lift $ doesDirectoryExist src
-    isDstDir <- lift $ doesDirectoryExist dst
-    when isSrcFile (cp src dst)
-    when (isSrcDir && (not isDstFile))
-        (if (length (dst:s)) /= 2 then lift (usage [])
-            else cpdir src dst)
-    when (isSrcFile && isDstDir) (cp src dst)
-    copy srcs dst
+copy src@(s:ss) dst = do
+    isSrcFile <- liftIO $ doesFileExist s
+    isDstFile <- liftIO $ doesFileExist dst
+    isSrcDir <- liftIO $ doesDirectoryExist s
+    isDstDir <- liftIO $ doesDirectoryExist dst
+    when (not isDstFile && not isDstDir) -- file or dir to dne
+        (if isSrcFile then cp s dst
+            else if isDstFile then cpdir s dst
+                else liftIO $ usage [])
+    when (isSrcFile && isDstDir) (cp s dst)
+    when (isSrcDir && isDstFile) (liftIO $ usage []) -- dir to file
+    when (isSrcDir && isDstDir) (cpdir s dst) -- dir to dir
+    when (isSrcFile && isDstFile) (cp s dst) -- file to file
+    copy ss dst
 
-cp :: String -> String -> ReaderT [Flag] IO ()
-cp src dst = do
-    opts <- ask
-    isDstDir <- lift $ doesDirectoryExist dst
-    let dst' = if isDstDir then dst ++ "/" ++ src
-               else dst
-    lift $ copyFile src dst'
-    lift $ when (Verbose `elem` opts) 
-        (verbose src dst')
-
-cpdir :: String -> String -> ReaderT [Flag] IO ()
+cpdir :: String -> String -> CopyMonad
 cpdir src dst = do
     opts <- ask
-    isDstDir <- lift $ doesDirectoryExist dst
-    dotdot <- lift $ getDirectoryContents src
-    let dst' = if isDstDir then dst ++ "/" ++ src
-               else dst
-        basename = filter (`notElem` [".",".."]) dotdot
-        contents = map ((dst'++"/")++) basename
-    lift $ when (Recurse `notElem` opts) (usage [])
-    lift $ createDirectoryIfMissing False dst'
+    dotdot <- liftIO $ getDirectoryContents src
+    when (Recurse `notElem` opts) 
+        (liftIO $ usage (src ++ " is directory!"))
+    let dst' = if last dst == '/'
+               then dst
+               else dst ++ "/"
+        src' = if last src == '/'
+               then src
+               else src ++ "/"
+        dotdot' = filter (`notElem` [".",".."]) dotdot
+        contents = map (src'++) dotdot'
+    liftIO $ createDirectoryIfMissing False dst'
     copy contents dst'
+
+cp :: String -> String -> CopyMonad
+cp src dst = do
+    opts <- ask
+    let dst' = if last dst == '/'
+               then dst ++ src
+               else dst ++ "/" ++ src
+    liftIO $ copyFile src dst'
+    when (Verbose `elem` opts) (liftIO $ verbose src dst')
 
 verbose :: String -> String -> IO ()
 verbose src dst = printf "cp: %s\t%s\n" src dst
@@ -63,13 +71,17 @@ main :: IO ()
 main = do
     argv <- getArgs
     case getOpt RequireOrder flags argv of
-        (opts,args,[]) -> runReaderT 
-            (copy (init args) (last args)) opts
+        (opts,args,[]) -> do
+            res <- runErrorT $ runReaderT 
+                (copy (init args) (last args)) opts
+            case res of
+                (Right _) -> return ()
+                (Left err) -> print err
         (_,_,err) -> usage $ concat err
 
 usage :: String -> IO ()
-usage err = do 
-    unless (null err) (hPutStrLn stderr err)
+usage errstr = do
+    unless (null errstr) (hPutStrLn stderr errstr)
     hPutStr stderr $ "usage: cp [-rv] file1 file2\n" ++
                      "usage: cp [-rv] file ... directory\n" ++
                      "usage: cp [-rv] dir1 dir2\n"
